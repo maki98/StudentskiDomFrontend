813cd5149db717c0863c59055de38de442e512a9
maki98 <karanovicm57@gmail.com>
2020-06-12 01:41:23 +0200

    Dodati dugmici za logout i za profil, namestena pocetna stranica

---
+++ src/app/components/home/home-sluzbenici/home-sluzbenici.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-home-sluzbenici',
+  templateUrl: './home-sluzbenici.component.html',
+  styleUrls: ['./home-sluzbenici.component.css']
+})
+export class HomeSluzbeniciComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
